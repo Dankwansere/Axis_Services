b2c521f2301273906d429c6dd9746f5148f38627
Eric <ericsarpong@hotmail.com>
2018-08-09 20:12:11 -0400

    Class to handle custom response codes

---
+++ src/main/java/com/sans/axis/commons/AxisResponseCodes.java
@@ -0,0 +1,8 @@
+package com.sans.axis.commons;
+
+public class AxisResponseCodes {
+	public static final String STATUS_VALID = "Valid";
+	public static final String STATUS_INVALID = "Invalid";
+	
+
+}
