0f69eae45b4ea3e4323c120113982eac3aa9cebd
Eric <ericsarpong@hotmail.com>
2018-08-11 15:18:05 -0400

    pojo to handle single info validation
    pojo to handle single info validation

---
+++ src/main/java/com/sans/axis/domain/UserSingleInfoValidator.java
@@ -0,0 +1,21 @@
+package com.sans.axis.domain;
+
+public class UserSingleInfoValidator {
+	
+	private String value;
+	private int validatorType;
+	
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public int getValidatorType() {
+		return validatorType;
+	}
+	public void setValidatorType(int validatorType) {
+		this.validatorType = validatorType;
+	}
+
+}
