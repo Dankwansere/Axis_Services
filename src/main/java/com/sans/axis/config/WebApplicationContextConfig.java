238fd73baa56f3b584ca0d181a6a52f816f0455a
Eric <ericsarpong@hotmail.com>
2018-08-11 15:15:26 -0400

    Cors configuration
    Cors configuration

--- src/main/java/com/sans/axis/config/WebApplicationContextConfig.java
+++ src/main/java/com/sans/axis/config/WebApplicationContextConfig.java
@@ -4,8 +4,10 @@ import org.springframework.boot.web.support.ErrorPageFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -27,6 +29,16 @@ public class WebApplicationContextConfig extends WebMvcConfigurerAdapter {
 	    filterRegistrationBean.setEnabled(false);
 	    return filterRegistrationBean;
 	} */
+	
+	 @Bean
+	    public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurerAdapter() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("*").allowedOrigins("*");
+	            }
+	        };
+	    }
 	
 	
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
