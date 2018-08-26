a0f35bc97dfbd4215d96264440e1c85ac340b37d
Eric <ericsarpong@hotmail.com>
2018-08-26 14:31:45 -0400

    IEmployeeService interface
    IEmployeeService interface

---
+++ src/main/java/com/sans/axis/service/IEmployeeService.java
@@ -0,0 +1,9 @@
+package com.sans.axis.service;
+
+import com.sans.axis.domain.Employee;
+import com.sans.axis.domain.User;
+
+public interface IEmployeeService {
+	
+	public Employee createEmployee(User user);
+}
