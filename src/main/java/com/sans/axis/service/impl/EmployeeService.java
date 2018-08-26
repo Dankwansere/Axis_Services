a7fb8266da6b78bdebdb51e4e34f112c4d772253
Eric <ericsarpong@hotmail.com>
2018-08-26 14:32:29 -0400

    EmployeeService implementation
    EmployeeService implementation

---
+++ src/main/java/com/sans/axis/service/impl/EmployeeService.java
@@ -0,0 +1,24 @@
+package com.sans.axis.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sans.axis.domain.Employee;
+import com.sans.axis.domain.User;
+import com.sans.axis.domain.repository.IEmployeeCustomRepository;
+import com.sans.axis.domain.repository.IEmployeeRepository;
+import com.sans.axis.service.IEmployeeService;
+
+@Service
+public class EmployeeService implements IEmployeeService {
+
+	@Autowired
+	private IEmployeeCustomRepository employeeRepository;
+	
+	@Override
+	public Employee createEmployee(User user) {
+		
+		return this.employeeRepository.createEmployee(user);
+	}
+
+}
