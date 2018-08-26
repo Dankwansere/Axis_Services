ea85cb14366930f91e3056d58856b006ffd35fb5
Eric <ericsarpong@hotmail.com>
2018-08-26 14:26:57 -0400

    removed unused imports
    removed unused imports

--- src/main/java/com/sans/axis/controller/TimesheetController.java
+++ src/main/java/com/sans/axis/controller/TimesheetController.java
@@ -3,9 +3,12 @@ package com.sans.axis.controller;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,7 @@ import com.sans.axis.domain.UserTimesheet;
 import com.sans.axis.service.ITimesheetService;
 
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequestMapping(value = "/timesheet/")
@@ -23,6 +27,7 @@ public class TimesheetController {
 	
 	@Autowired
 	private ITimesheetService timesheetService;
+	
 	
 	@RequestMapping(value = "submitTimesheetForm", method = RequestMethod.POST)
 	public Map<String, Boolean> submitTimeSheet(@RequestBody UserTimesheet[] userTimesheet) {
