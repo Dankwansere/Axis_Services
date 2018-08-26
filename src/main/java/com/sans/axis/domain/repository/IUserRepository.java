0b8072f280cbc758475a97554f1f78979a0c269b
Eric <ericsarpong@hotmail.com>
2018-08-26 14:31:24 -0400

    minor clean up
    minor clean up

--- src/main/java/com/sans/axis/domain/repository/IUserRepository.java
+++ src/main/java/com/sans/axis/domain/repository/IUserRepository.java
@@ -1,13 +1,15 @@
 package com.sans.axis.domain.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import com.sans.axis.domain.Employee;
 import com.sans.axis.domain.User;
 
 @Repository
 public interface IUserRepository extends CrudRepository<User, Long> {
-	
-	
 
 }
+
+
 
 
