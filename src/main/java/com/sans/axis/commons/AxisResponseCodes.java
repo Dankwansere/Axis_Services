e2ba2011b87b1efb17a55ffb81f2cc40a784cc1b
Eric <ericsarpong@hotmail.com>
2018-08-26 14:26:35 -0400

    new constants
    new constants

--- src/main/java/com/sans/axis/commons/AxisResponseCodes.java
+++ src/main/java/com/sans/axis/commons/AxisResponseCodes.java
@@ -4,5 +4,10 @@ public class AxisResponseCodes {
 	public static final String STATUS_VALID = "Valid";
 	public static final String STATUS_INVALID = "Invalid";
 	
+	public static final String USER_ALREADY_EXISTS = "User already exists";
+	public static final String EMAIL_ALREADY_EXISTS = "Email ID already exists";
+	public static final String CREATE_REQUIRED_NAMES = "Username, first name, last name cannot be empty";
+	public static final String CREATE_EMAIL_EMPTY = "Email cannot be empty";
+
 
 }
