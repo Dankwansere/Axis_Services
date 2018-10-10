e10d79bc486bf02f02e59a7ab90a8217815c4093
Eric <ericsarpong@hotmail.com>
2018-10-09 22:03:34 -0400

    overloaded constructor and static method

--- src/main/java/com/sans/axis/domain/AxisResponse.java
+++ src/main/java/com/sans/axis/domain/AxisResponse.java
@@ -1,14 +1,31 @@
 package com.sans.axis.domain;
 
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+
 public class AxisResponse {
 	
 	private String status;
 	private Object data;
 	private String exception;
 	
+	
+	
+	public AxisResponse() {
+		super();
+	}
+
+	public AxisResponse(String status, Object data, String exception) {
+		super();
+		this.status = status;
+		this.data = data;
+		this.exception = exception;
+	}
+
+
 	public String getStatus() {
 		return status;
 	}
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
@@ -23,6 +40,22 @@ public class AxisResponse {
 	}
 	public void setException(String exception) {
 		this.exception = exception;
+	}
+	
+	public static AxisResponse setAxisResponse(String status) {
+		return setAxisResponse(status, null);
+	}
+	
+	public static AxisResponse setAxisResponse(String status, Object data) {
+		return setAxisResponse(status, data, "");
+	}
+	
+	public static AxisResponse setErrorAxisResponse(String status, String exception) {
+		return setAxisResponse(status, null, exception);
+	}
+	
+	public static AxisResponse setAxisResponse(String status, Object data, String exception) {
+		return new AxisResponse(status, data, exception);
 	}
 
 }
