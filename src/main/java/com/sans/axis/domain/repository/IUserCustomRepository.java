59a761bac68e6012580a5fdcda734104dffa4f89
Eric <ericsarpong@hotmail.com>
2018-10-09 22:04:23 -0400

    exception to create user method

--- src/main/java/com/sans/axis/domain/repository/IUserCustomRepository.java
+++ src/main/java/com/sans/axis/domain/repository/IUserCustomRepository.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Repository;
 
+import com.sans.axis.commons.AxisException;
 import com.sans.axis.domain.GenericControlList;
 import com.sans.axis.domain.User;
 
@@ -11,12 +12,14 @@ import com.sans.axis.domain.User;
 public interface IUserCustomRepository {
 	
 	public User getUser(String userName, String passWord);
+	
+	public User findByUsername(String username);
 		
 	public boolean validateUserName(String username);
 	
 	public boolean validateEmail(String email);
 		
-	public User createUser(User user);
+	public User createUser(User user) throws AxisException;
 	
 	ArrayList<GenericControlList> getUserProjectList();
 
