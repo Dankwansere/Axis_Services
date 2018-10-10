f38662f5e4f9f249e3877f090066b434d78e8823
Eric <ericsarpong@hotmail.com>
2018-10-09 22:02:24 -0400

    updated response and added token to login response

--- src/main/java/com/sans/axis/controller/UserController.java
+++ src/main/java/com/sans/axis/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -20,6 +21,7 @@ import com.sans.axis.domain.User;
 import com.sans.axis.domain.UserSingleInfoValidator;
 import com.sans.axis.service.IEmployeeService;
 import com.sans.axis.service.IUserService;
+import com.sans.axis.service.impl.JwtService;
 import com.sans.axis.commons.AxisException;
 import com.sans.axis.commons.AxisResponseCodes;
 
@@ -43,21 +45,23 @@ public class UserController {
 	public ResponseEntity<AxisResponse> getUser(@RequestBody User user) {
 		
 		this.axisResponse = new AxisResponse();
+		HttpHeaders responseHeaders = new HttpHeaders();
+		
 		
 		try {
 			this.user = userService.getUser(user.getUsername(), user.getPassword());
 			if(this.user == null) {
 				this.axisResponse.setStatus(AxisResponseCodes.STATUS_INVALID);
-				return new ResponseEntity<AxisResponse>(this.axisResponse, HttpStatus.OK);
+				return new ResponseEntity<AxisResponse>(this.axisResponse, HttpStatus.OK) ;
 			} else {
-				this.axisResponse.setData(this.user);
-				this.axisResponse.setStatus(AxisResponseCodes.STATUS_VALID);
-				return new ResponseEntity<AxisResponse>(this.axisResponse, HttpStatus.OK);
+				responseHeaders.add("Authorization", JwtService.genJwtToken(user));
+				responseHeaders.add("access-control-expose-headers", "Authorization");
+				this.axisResponse = AxisResponse.setAxisResponse(AxisResponseCodes.STATUS_VALID, this.user);
+				return new ResponseEntity<AxisResponse>(this.axisResponse, responseHeaders, HttpStatus.OK) ;
 			}
 			
 		} catch (Exception ex) {
-			this.axisResponse.setStatus("Error");
-			this.axisResponse.setException(ex.getMessage());
+			this.axisResponse = AxisResponse.setErrorAxisResponse("Error", ex.getMessage());
 			return new ResponseEntity<AxisResponse>(this.axisResponse, HttpStatus.SERVICE_UNAVAILABLE);
 			
 		}
@@ -68,16 +72,13 @@ public class UserController {
 	@ResponseStatus(value = HttpStatus.OK)
 	public ResponseEntity<AxisResponse> ValidateSingleUserInfo(@RequestBody UserSingleInfoValidator info) {
 		this.axisResponse = new AxisResponse();
-		System.out.println("Validator type: " + info.getValidatorType());;
-		System.out.println("Received username: " + info.getValue());
 		boolean isInfoExist = false;
 		
-		//validate username
+		//validate user name
 		if(info.getValidatorType() == 0) {
 			try {
 				isInfoExist = userService.validateUserName(info.getValue());
-				this.axisResponse.setStatus(AxisResponseCodes.STATUS_VALID);
-				this.axisResponse.setData(Collections.singletonMap("isInfoExist", isInfoExist));
+				this.axisResponse = AxisResponse.setAxisResponse(AxisResponseCodes.STATUS_VALID, Collections.singletonMap("isInfoExist", isInfoExist));
 				
 			} catch (Exception ex) {
 				this.axisResponse.setStatus(AxisResponseCodes.STATUS_INVALID);
@@ -86,15 +87,13 @@ public class UserController {
 		} else if(info.getValidatorType() == 1) {
 			try {
 				isInfoExist = userService.validateEmail(info.getValue());
-				this.axisResponse.setStatus(AxisResponseCodes.STATUS_VALID);
-				this.axisResponse.setData(Collections.singletonMap("isInfoExist", isInfoExist));
+				this.axisResponse = AxisResponse.setAxisResponse(AxisResponseCodes.STATUS_VALID, Collections.singletonMap("isInfoExist", isInfoExist));
+
 				
 			} catch(Exception ex) {
-				this.axisResponse.setStatus(AxisResponseCodes.STATUS_INVALID);
-				this.axisResponse.setException(ex.getMessage());
+				this.axisResponse = AxisResponse.setErrorAxisResponse(AxisResponseCodes.STATUS_INVALID, ex.getMessage());
 				return new ResponseEntity<AxisResponse>(this.axisResponse, HttpStatus.SERVICE_UNAVAILABLE);
 			}
-			
 		}
 	
 		return new ResponseEntity<AxisResponse>(this.axisResponse, HttpStatus.OK);	
@@ -105,19 +104,14 @@ public class UserController {
 	@ResponseStatus(value = HttpStatus.CREATED)
 	public ResponseEntity<AxisResponse> create(@RequestBody User user) {	
 		
-		System.out.println("User: " + user.toString());
-		
-		this.axisResponse = new AxisResponse();
-		
-		
+		this.axisResponse = new AxisResponse();		
 		try {
 			boolean isDataValid;
 			//validate user name
 			isDataValid = this.userService.validateUserName(user.getUsername());
 			if(isDataValid) {
 				this.axisException = new AxisException(AxisResponseCodes.USER_ALREADY_EXISTS);
-				this.axisResponse.setStatus(AxisResponseCodes.STATUS_INVALID);
-				this.axisResponse.setException(this.axisException.getMessage());
+				this.axisResponse = AxisResponse.setErrorAxisResponse(AxisResponseCodes.STATUS_INVALID, this.axisException.getMessage());
 				return new ResponseEntity<AxisResponse>(this.axisResponse, HttpStatus.OK);
 			}
 			
@@ -125,27 +119,33 @@ public class UserController {
 			isDataValid = this.userService.validateEmail(user.getEmail());
 			if(isDataValid) {
 				this.axisException = new AxisException(AxisResponseCodes.EMAIL_ALREADY_EXISTS);
-				this.axisResponse.setStatus(AxisResponseCodes.STATUS_INVALID);
-				this.axisResponse.setException(this.axisException.getMessage());
+				this.axisResponse = AxisResponse.setErrorAxisResponse(AxisResponseCodes.STATUS_INVALID, this.axisException.getMessage());
 				return new ResponseEntity<AxisResponse>(this.axisResponse, HttpStatus.OK);
 			}
 			
 			//If validation is success then create account
 			User userDTO = this.userService.createUser(user);
-			
+			Employee employeeDTO;
 			if(userDTO != null) {
-			 Employee employeeDTO =	this.employeeService.createEmployee(userDTO);
+				employeeDTO  =	this.employeeService.createEmployee(userDTO);
+			} else {
+				 this.axisResponse = AxisResponse.setErrorAxisResponse(AxisResponseCodes.STATUS_INVALID, this.axisException.getMessage());
+				 return new ResponseEntity<AxisResponse>(this.axisResponse, HttpStatus.OK);	
 			}
 			
+			if(employeeDTO != null) {
+				this.axisResponse = AxisResponse.setAxisResponse(AxisResponseCodes.STATUS_VALID, Collections.singletonMap("isUserCreated", true));
+				
+			} else {
+				this.axisResponse = AxisResponse.setAxisResponse(AxisResponseCodes.STATUS_INVALID, Collections.singletonMap("isUserCreated", true));
+			}
 			
 		} catch (NullPointerException ex) {
-			this.axisResponse.setStatus(AxisResponseCodes.STATUS_INVALID);
-			this.axisResponse.setException(ex.getMessage());
+			this.axisResponse = AxisResponse.setErrorAxisResponse(AxisResponseCodes.STATUS_INVALID, ex.getMessage());
 		}
 		
 		catch(Exception ex) {
-			this.axisResponse.setStatus(AxisResponseCodes.STATUS_INVALID);
-			this.axisResponse.setException(ex.getMessage());
+			this.axisResponse = AxisResponse.setErrorAxisResponse(AxisResponseCodes.STATUS_INVALID, ex.getMessage());
 			
 		}
 		return new ResponseEntity<AxisResponse>(this.axisResponse, HttpStatus.OK);	
